9531c860bb952bb7989c31a5cb82e4d726c3d1ff
Ben Ford <ben.fordnz@gmail.com>
2013-06-19 06:44:05 +0100

    Pull all categories into a Map
    Needs to be keyed off url really

--- src/Data/FreeAgent/Types/Categories.hs
+++ src/Data/FreeAgent/Types/Categories.hs
@@ -1,34 +1,51 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE DeriveDataTypeable  #-}
-{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TemplateHaskell    #-}
 module Data.FreeAgent.Types.Categories where
 
-import qualified Data.ByteString as BS
-import           Control.Applicative ((<$>), (<*>), empty)
+import           Control.Applicative (empty, (<$>), (<*>))
 import           Control.Lens
 import           Data.Aeson
 import           Data.Aeson.TH
+import qualified Data.ByteString     as BS
 import           Data.Data
+import qualified Data.Map            as M
 
-data IncomeCategories = IncomeCategories {
+data TransactionCategory = TransactionCategory {
     _url                 :: BS.ByteString -- https://api.freeagent.com/v2/categories/001
   , _description         :: BS.ByteString -- Sales
   , _nominal_code        :: BS.ByteString -- 001
-  , _auto_sales_tax_rate :: BS.ByteString -- Standard rate
+  , _auto_sales_tax_rate :: Maybe BS.ByteString -- Standard rate
+  , _tax_reporting_name  :: Maybe BS.ByteString
 
 } deriving (Show, Data, Typeable)
 
+type Categories = [TransactionCategory]
 
-instance FromJSON IncomeCategories where
-  parseJSON (Object v) = IncomeCategories <$>
-                     v .: "url" <*>
-                     v .: "description" <*>
-                     v .: "nominal_code" <*>
-                     v .: "auto_sales_tax_rate"
+data TransactionTypes = TransactionTypes {
+    _admin_expenses_categories :: Categories
+  , _cost_of_sales_categories :: Categories
+  , _income_categories :: Categories
+  , _general_categories :: Categories
+} deriving (Show, Data, Typeable)
+
+type CatMap = M.Map BS.ByteString TransactionCategory
+
+instance FromJSON TransactionCategory where
+  parseJSON (Object v) = TransactionCategory <$>
+                     v .:  "url" <*>
+                     v .:  "description" <*>
+                     v .:  "nominal_code" <*>
+                     v .:? "auto_sales_tax_rate" <*>
+                     v .
+                     
+                     :? "tax_reporting_name"
 
   parseJSON _            = empty
 
 
+$(deriveToJSON tail ''TransactionCategory)
+$(makeLenses      ''TransactionCategory)
 
-$(deriveToJSON tail ''IncomeCategories)
-$(makeLenses      ''IncomeCategories)
+$(deriveJSON tail ''TransactionTypes)
+$(makeLenses      ''TransactionTypes)
--- src/Web/FreeAgent.hs
+++ src/Web/FreeAgent.hs
@@ -1,26 +1,55 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoMonomorphismRestriction  #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TemplateHaskell            #-}
+
 module Web.FreeAgent where
 
-import           Control.Monad.IO.Class          (MonadIO)
-import qualified Data.ByteString.Char8           as C8
-import           Data.Data
-
+import           Control.Applicative
+import           Control.Lens
+import           Control.Monad.IO.Class            (MonadIO)
+import           Control.Monad.Trans.Either
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.Aeson.Types
+import qualified Data.ByteString.Char8             as C8
+import           Data.FreeAgent.Types.BankAccounts
+import           Data.FreeAgent.Types.Categories
+import           Data.FreeAgent.Types.Company
+import           Data.FreeAgent.Types.Expenses
+import qualified Data.Map as M
 import           Network.OAuth.OAuth2
 import           Network.OAuth.OAuth2.HttpClient
-
-
 import           Web.FreeAgent.OAuth2
 import           Web.FreeAgent.Types
 import           Web.FreeAgent.Util
--- a custom request
 
 
-getCompany
-  :: (Data.Data.Data b, MonadIO m) => C8.ByteString -> FAMonad m b
-getCompany = apiCall "company"
+t :: Token
+t = "1qct_y3hb_AzVzFu6S4ndNx6UDDZuHYALqLkm5T69"
 
+data CompanyResp = CompanyResp { company :: Company }
+  deriving Show
+  
+data ExpensesResp = ExpensesResp { expenses :: Expenses}
+
+$(deriveJSON id ''CompanyResp)
+$(deriveJSON id ''ExpensesResp)
+
+getCompany :: (MonadIO m) => Token -> FAMonad m Company
+getCompany t = apiCall t "company" >>= extractApiVal "company"
+
+getBa :: (MonadIO m) => Token -> FAMonad m Value
+getBa t = apiCall t "bank_accounts/207811"
+
+getAccount :: FAMonad IO BankAccount
+getAccount = getBa t >>= FAMonad . hoistEither . parseEither (extractVal "bank_account")
+
+getExpenses :: MonadIO m => Token -> FAMonad m Expenses
+getExpenses t = apiCall t "expenses" >>= extractApiVal "expenses"
+
+getExps :: MonadIO m => Token -> FAMonad m ExpensesResp
+getExps t = apiCall t "expenses"
 
 getAccessToken :: IO Token
 getAccessToken = do
@@ -30,3 +59,16 @@ getAccessToken = do
   (Just (AccessToken accessToken Nothing)) <- requestAccessToken faKey code
   print accessToken
   return accessToken
+  
+-- getCategories :: MonadIO m => FAMonad m CatMap
+getCategories = catMapTypes <$> apiCall t "categories"
+    
+catMapTypes :: TransactionTypes -> CatMap
+catMapTypes (TransactionTypes admin cos income general) = M.fromList $ map extractKV allTypes
+  where
+    allTypes = concat [admin, cos, income, general]
+    extractKV tt = (tt^.nominal_code, tt)
+
+printExpenses = do
+  (Right exp) <- runFreeAgent $ getExpenses t
+  return $ map (\e -> (e^.gross_value, e^.category)) exp
