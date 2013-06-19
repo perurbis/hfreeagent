37587f3147097315ea9da04d8d21c9fd5f3cc9bd
Ben Ford <ben.fordnz@gmail.com>
2013-06-19 06:43:15 +0100

    Add TH to the AccessToken

--- src/Web/FreeAgent/Types.hs
+++ src/Web/FreeAgent/Types.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleInstances  #-}
 {-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TemplateHaskell    #-}
 
 module Web.FreeAgent.Types where
 
-import qualified Data.ByteString.Char8     as BS
+import           Data.Aeson.TH
+import qualified Data.ByteString.Char8 as BS
 import           Data.Data
 
 
 type Token = BS.ByteString
 
 data AccessTokenResp = AccessTokenResp {
-       access_token  :: BS.ByteString
-     , token_type    :: BS.ByteString
-     , expires_in    :: Int
+       _access_token  :: BS.ByteString
+     , _token_type    :: BS.ByteString
+     , _expires_in    :: Int
      --, refresh_token :: BS.ByteString
      } deriving (Show, Data, Typeable)
+
+$(deriveJSON tail ''AccessTokenResp)
